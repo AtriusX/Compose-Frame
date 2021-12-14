bf3a277e00fc62afab15f65ff406f7cf58b19ca6
Atrius <atriusedm@gmail.com>
2021-12-14 02:19:45 -0500

    Key detection component for running a code block on a certain keypress

---
+++ src/main/kotlin/ui/component/Submit.kt
@@ -0,0 +1,25 @@
+package ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun Submit(
+    modifier: Modifier = Modifier,
+    key: Key = Key.Enter,
+    onSubmit: () -> Unit = {},
+    content: @Composable () -> Unit = {}
+) {
+    Box(modifier.onKeyEvent {
+        if (it.key == key) onSubmit()
+        false
+    }) {
+        content()
+    }
+}
